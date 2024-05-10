496b59e71f3ca23f280aa180b5056ed7f1fad498
Sangyong Gwak <imyaman@netscape.net>
2024-05-10 16:57:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'Hello world with Flask'
+ 
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
+ 
+if __name__ == '__main__':
+   app.run()
