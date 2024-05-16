6ab95507830b5cf4895815f1e9a08f478424558e
Sangyong Gwak <imyaman@netscape.net>
2024-05-16 22:51:19 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-apk update  # py3-flask 
-apk add sed perl perl-mojolicious perl-app-cpanminus && cpanm --cpanfile cpanfile
+apk update && apk add sed perl perl-mojolicious perl-app-cpanminus py3-flask py3-jinja2 && cpanm --cpanfile cpanfile
+pip install -r requirements.txt
